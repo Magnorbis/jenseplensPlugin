c1e1e9b65d4f672e17b4dc5d6c8032a3cf4fc9ec
Mart Koster <the947thtokay@codingtime.nl>
2018-05-10 12:07:18 +0200

    Rename Main

--- src/me/jenseplens/jenseplensPlugin/JenseplensPlugin.java
+++ src/me/jenseplens/jenseplensPlugin/JenseplensPlugin.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class JenseplensPlugin extends JavaPlugin {
 	public void onEnable() {}
 	
     @Override
