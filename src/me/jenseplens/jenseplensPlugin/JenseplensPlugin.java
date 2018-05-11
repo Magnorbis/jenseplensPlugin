44b11fde52ee3e720b568707e5e849e819d720f7
Mart Koster <the947thtokay@codingtime.nl>
2018-05-11 16:33:38 +0200

    Remove console check for jenseplens, staff and jenseplensreloadcommand, fixes #2

--- src/me/jenseplens/jenseplensPlugin/JenseplensPlugin.java
+++ src/me/jenseplens/jenseplensPlugin/JenseplensPlugin.java
@@ -25,7 +25,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class JenseplensPlugin extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        //staff Command
+        // staff Command
         if(cmd.getName().equalsIgnoreCase("staff")) {
             sender.sendMessage(ChatColor.GREEN.toString() + "Eigenaar: jenseplens");
             sender.sendMessage(ChatColor.GREEN.toString() + "Administrator: SplashIV");
@@ -33,29 +33,29 @@ public class JenseplensPlugin extends JavaPlugin {
             sender.sendMessage(ChatColor.GREEN.toString() + "Bouwer: MelsBov");
         }
 
-        //jenseplens Command
-        if(cmd.getName().equalsIgnoreCase("jenseplens")) {
+        // jenseplens Command
+        else if(cmd.getName().equalsIgnoreCase("jenseplens")) {
             sender.sendMessage(ChatColor.GREEN.toString() + "jenseplens is de eigenaar van deze server");
         }
 
-        //plugin Reload Command
-        if(cmd.getName().equalsIgnoreCase("jenseplenspluginreload") && sender.hasPermission("jenseplensplugin.reload")) {
+        // plugin Reload Command
+        else if(cmd.getName().equalsIgnoreCase("jenseplenspluginreload") && sender.hasPermission("jenseplensplugin.reload")) {
             this.reloadConfig();
             sender.sendMessage("Toppie, gereload.");
         }
 
-        if(!(sender instanceof Player)) {
-            sender.sendMessage("Dit commando kan niet worden uitgevoerd door de console!");
-            return true;
-        }
-        Player p = (Player) sender;
-
-        //locatie Command
-        if(cmd.getName().equalsIgnoreCase("locatie")) {
+        // locatie Command (can only be executed by players)
+        else if(sender instanceof Player && cmd.getName().equalsIgnoreCase("locatie")) {
+            Player p = (Player) sender;
             int spelerx = p.getLocation().getBlockX();
             int spelery = p.getLocation().getBlockY();
             int spelerz = p.getLocation().getBlockZ();
             p.sendMessage(ChatColor.GREEN.toString() + "x: " + spelerx + " - y: "+ spelery + " - z: " + spelerz);
+        }
+
+        // Executed by console, cannot find location
+        else {
+            sender.sendMessage("Dit commando kan niet worden uitgevoerd door de console!");
         }
 
         return true;
